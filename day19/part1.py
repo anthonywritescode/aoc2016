6869340ddabf8bddfb7165d922431a1e417d66d4
Anthony Sottile <asottile@umich.edu>
2024-11-04 19:47:34 -0500

    day 19 part 1

---
+++ day19/part1.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import argparse
+import collections
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    d = collections.deque(range(int(s)))
+    while len(d) != 1:
+        d.rotate(-1)
+        d.popleft()
+
+    return d[0] + 1
+
+
+INPUT_S = '''\
+5
+'''
+EXPECTED = 3
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
